b5a27049ec886b77665c4136e4db1eed9d9fb592
Filippov-Nikolay <filippovnikolay195@gmail.com>
2023-09-07 11:30:00 +0300

    Добавьте файлы проекта.

---
+++ lesson4 07.07.2023/lesson4 07.07.2023.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Point {
+private:
+	int x;
+	int y;
+
+public:
+	Point() { // Конструктор без параметров будет по умолчанию
+		cout << "Constructor by default" << endl;
+		x = y = 0;
+	}
+
+	Point(int a, int b) { // Конструктор с параметром
+		cout << "Constructor by 2 params" << endl;
+		x = a;
+		y = b;
+	}
+
+	Point(int a) { // Конструктор с параметром
+		cout << "Constructor by 1 params" << endl;
+		x = y = a;
+	}
+
+	~Point() {
+		cout << "Destructor" << endl;
+	}
+
+
+	void Init(int a, int b) {
+		x = a;
+		y = b;
+	}
+
+	void Print() {
+		cout << "X: " << x << " Y: " << y << endl;
+	}
+};
+
+int main() {
+	Point obj1; // Выозов конструктора по умолчанию!
+	obj1.Print();
+
+	obj1.Init(15, 20);
+	obj1.Print();
+
+
+	Point obj2(100, 200); // Выозов конструктора с параметрами
+	obj2.Print();
+
+	obj2.Init(5, 8);
+	obj2.Print();
+
+
+	Point obj3(5);
+	obj3.Print();
+
+   return 0;
+}
---
+++ lesson4 07.07.2023/student.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+using namespace std;
+
+/* добавить:
+	   * поле емайл(char*)
+	   * конструктор  с 3я параметрами
+	   * методы аксессоры
+	   * удаление выделенной динамической памяти в деструкторе.
+*/
+
+class Student
+{
+	char* name;
+	char* email;
+	int age;
+public:
+	Student()
+	{
+		cout << "Constructor by def.\n";
+		name = nullptr;
+		email = nullptr;
+		age = 0;
+	}
+	Student(const char* n, int a, const char* e)
+	{
+		name = new char[strlen(n) + 1]; // выделяем память при создани  объекта !!
+		strcpy_s(name, strlen(n) + 1, n);
+		email = new char[strlen(e) + 1];
+		strcpy_s(email, strlen(e) + 1, e);
+		age = a;
+		cout << "Constructor 3 params\n";
+	}
+	Student(const char* n, int a)
+	{
+		name = new char[strlen(n) + 1]; // выделяем память при создани  объекта !!
+		strcpy_s(name, strlen(n) + 1, n);
+		age = a;
+		cout << "Constructor 2 params\n";
+	}
+	void Input()
+	{
+		char buff[20];
+		cout << "Enter name: ";
+		cin >> buff;
+		if (name != nullptr)
+		{
+			cout << "Delete -> " << name << "." << endl;
+			delete[]name;
+		}
+		name = new char[strlen(buff) + 1];
+		strcpy_s(name, strlen(buff) + 1, buff);
+
+		cout << "Enter age: ";
+		cin >> age;
+	}
+
+	void FullInput() {
+		char buff[20];
+		cout << "Enter name: ";
+		cin >> buff;
+		if (name != nullptr)
+		{
+			cout << "Delete -> " << name << "." << endl;
+			delete[]name;
+		}
+		name = new char[strlen(buff) + 1];
+		strcpy_s(name, strlen(buff) + 1, buff);
+
+		cout << "Enter age: ";
+		cin >> age;
+
+		cout << "Enter email: ";
+		cin >> buff;
+
+		if (email != nullptr)
+		{
+			cout << "Delete -> " << name << "." << endl;
+			delete[]email;
+		}
+
+		email = new char[strlen(buff) + 1];
+		strcpy_s(email, strlen(buff) + 1, buff);
+	}
+
+	void Print()
+	{
+		cout << "Name: " << name << "\tAge: " << age << endl;
+	}
+	void FullPrint() {
+		cout << "Name: " << name << "\nAge: " << age << "\nemail: " << email << endl;
+	}
+	~Student()
+	{
+		delete[] name; // явно освобождаем память при уничтожении объекта !!!!
+		delete[] email;
+		cout << "Destructor\n";
+	}
+};
+
+int main() {
+	Student obj1("Oleg", 20);
+	obj1.Print();
+
+	Student obj2("Nikolay", 20, "email@gmail.com");
+	obj2.FullPrint();
+
+	obj2.FullInput();
+	obj2.FullPrint();
+}
